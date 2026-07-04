fd93d2e40ec1c043c1cd67819e11fe2213c4467c
Sushmitha Chowdary <sushmithachowdary1502@gmail.com>
2026-07-04 10:57:45 +0530

    Added Singleton and Factory pattern code

---
+++ Module 1/singleton/Singleton.java
@@ -0,0 +1,21 @@
+package singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton() {
+        System.out.println("Singleton object created.");
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void display() {
+        System.out.println("This is Singleton Pattern.");
+    }
+}
