7899c29a36fade7aa3b8a0297a8761ab24747aae
Sushmitha Chowdary <sushmithachowdary1502@gmail.com>
2026-07-07 11:48:56 +0530

    Completed Singleton Pattern Exercise

---
+++ Module 1/Excercise-1-Singleton Pattern/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+public static void main(String[]args){
+Logger logger1=Logger.getInstance();
+Logger logger2=Logger.getInstance();
+
+logger1.log("Application Started");
+logger2.log("Application Running");
+
+if(logger1==logger2){
+System.out.println("Singleton Pattern Implemented Successfully");
+}else{
+System.out.println("Singleton Pattern Failed");
+}
+}
+}
