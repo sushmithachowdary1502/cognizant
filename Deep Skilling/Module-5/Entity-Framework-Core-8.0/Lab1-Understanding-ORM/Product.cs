fe02c1bba70e59a4835b79928ecbdc22b6af270b
Sushmitha Chowdary <sushmithachowdary1502@gmail.com>
2026-07-13 19:48:18 +0530

    Add Product class with properties and key attribute

---
+++ Deep Skilling/Module-5/Entity-Framework-Core-8.0/Lab1-Understanding-ORM/Product.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
