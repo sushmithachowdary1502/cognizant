c6f128f7f439c670e06c8c050c02a39bbde5ce11
Sushmitha Chowdary <sushmithachowdary1502@gmail.com>
2026-07-13 18:44:56 +0530

    Implement Singleton pattern in Main.java

---
+++ Deep Skilling/Module 1/singleton/Main.java
@@ -0,0 +1,15 @@
+package singleton;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        obj1.display();
+
+        if (obj1 == obj2)
+            System.out.println("Both objects are the same.");
+    }
+}
