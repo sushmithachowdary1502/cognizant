c808615f3cefd70ffcc83febffb8000aef44b58e
Sushmitha Chowdary <sushmithachowdary1502@gmail.com>
2026-07-13 19:29:05 +0530

    Add Product class for ecommerce module

---
+++ Deep Skilling/Module-2/Search_Function/Product.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+public class Product {
+    int id;
+    String name;
+    double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
